445a783fff49be5d2a016315bac1e8ddf515676b
jocker-py <buladzislau@gmail.com>
2022-11-13 17:18:47 +0300

    feat: add Profile page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import { Routes, Route } from 'react-router-dom';
 import WelcomePage from './pages/WelcomePage';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
+import Profile from './pages/Profile';
+import Boards from './pages/Boards';
 import { AppRoutes } from './constants/routes';
+import NotFound from "./pages/NotFound";
 
 const App: FC = () => {
   return (
@@ -17,6 +20,9 @@ const App: FC = () => {
         <Route path={AppRoutes.WELCOME} element={<WelcomePage />} />
         <Route path={AppRoutes.LOGIN} element={<Login />} />
         <Route path={AppRoutes.REGISTRATION} element={<Registration />} />
+        <Route path={AppRoutes.PROFILE} element={<Profile />}/>
+        <Route path={AppRoutes.BOARDS} element={<Boards />}/>
+        <Route path={AppRoutes.NOTFOUND} element={<NotFound />}/>
       </Routes>
       <Footer />
     </div>
