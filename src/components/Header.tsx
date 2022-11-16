537e4a5c6c688fac0d23419c7eabe351e772ca55
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-11-16 14:19:39 +0300

    feat: centering header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import { AppBar, Button, Container, Toolbar } from '@mui/material';
 import LanguageSwitcher from './LanguageSwitcher';
 import AuthButtonsContainer from './AuthButtonsContainer';
-import MainButton from './MainButton';
 import { AppRoutes } from '../constants/routes';
 import ModalWindow from './ModalWindow';
 import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
 import AccountBoxRoundedIcon from '@mui/icons-material/AccountBoxRounded';
+import HomeIcon from '@mui/icons-material/Home';
 
 const fontSize = '1.8rem';
 
@@ -25,22 +25,29 @@ const Header: FC<props> = ({ isLogin }) => {
   return (
     <AppBar position="static" component="header">
       <Container>
-        <Toolbar>
-          <MainButton onClick={onMainPage}>Главная</MainButton>
-          {isLogin && (
-            <>
-              <Button color="inherit" sx={{ fontSize: '1.8rem' }} onClick={onBoardsPage}>
-                <DashboardRoundedIcon sx={{ fontSize: '1.2em', marginRight: '5px' }} />
-                Доски
-              </Button>
-              <Button color="inherit" sx={{ fontSize: '1.8rem' }} onClick={onProfilePage}>
-                <AccountBoxRoundedIcon sx={{ fontSize: '1.2em', marginRight: '5px' }} />
-                Профиль
-              </Button>
-            </>
-          )}
-          <LanguageSwitcher fontSize={fontSize} />
-          <AuthButtonsContainer isLogin={isLogin} fontSize={fontSize} />
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Button color="inherit" sx={{ fontSize: '1.8rem' }} onClick={onMainPage}>
+            <HomeIcon sx={{ fontSize: '1.2em', marginRight: '5px' }} />
+            Главная
+          </Button>
+          <div>
+            {isLogin && (
+              <>
+                <Button color="inherit" sx={{ fontSize: '1.8rem' }} onClick={onBoardsPage}>
+                  <DashboardRoundedIcon sx={{ fontSize: '1.2em', marginRight: '5px' }} />
+                  Доски
+                </Button>
+                <Button color="inherit" sx={{ fontSize: '1.8rem' }} onClick={onProfilePage}>
+                  <AccountBoxRoundedIcon sx={{ fontSize: '1.2em', marginRight: '5px' }} />
+                  Профиль
+                </Button>
+              </>
+            )}
+          </div>
+          <div>
+            <LanguageSwitcher fontSize={fontSize} />
+            <AuthButtonsContainer isLogin={isLogin} fontSize={fontSize} />
+          </div>
         </Toolbar>
       </Container>
       <ModalWindow />
