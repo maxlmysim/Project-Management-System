bbed349cc1c36190ee462a6cf90c6a6afcc7de5d
jocker-py <buladzislau@gmail.com>
2022-11-29 20:16:24 +0300

    refactor: refcator buttons on TooltipButton

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Box, Button, Card, CardContent, Typography } from '@mui/material';
 import { IColumnResponse } from '../types/responseTypes';
 import AddButton from './Buttons/AddButton';
-import EditButton from './Buttons/EditButton';
-import DeleteButton from './Buttons/DeleteButton';
-import InfoButton from './Buttons/InfoButton';
+import SearchIcon from '@mui/icons-material/Search';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import TooltipButton from './TooltipButton';
 
 interface IColumnProps {
   column: IColumnResponse;
@@ -16,36 +18,66 @@ const style = {
   minHeight: 100,
   cursor: 'pointer',
   flexShrink: 0,
-  position: 'relative',
+  overflow: 'visible',
+};
+
+const styleContent = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+  flexShrink: 0,
 };
 
 const Column: FC<IColumnProps> = ({ column }) => {
+  const [showOptions, setShowOptions] = useState(false);
+  const toggleShowOptions = () => {
+    setShowOptions(!showOptions);
+  };
+  const closeShowOptions = () => {
+    setShowOptions(false);
+  };
   return (
     <Card sx={style} onClick={(): void => {}}>
-      <CardContent>
+      <CardContent sx={styleContent}>
         <Button
           variant="contained"
           color="secondary"
-          size="large"
+          size="medium"
           sx={{ fontSize: '1.6rem', width: '100%' }}
+          onBlur={closeShowOptions}
         >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              position: 'absolute',
+              top: '15%',
+              right: '5%',
+              zIndex: 1,
+            }}
+          >
+            <TooltipButton
+              title=""
+              handler={toggleShowOptions}
+              icon={<MoreVertIcon />}
+              backgroundColor="transparent"
+            />
+            {showOptions && (
+              <>
+                <TooltipButton title="Описание" handler={() => {}} icon={<SearchIcon />} />
+                <TooltipButton title="Изменить" handler={() => {}} icon={<EditIcon />} />
+                <TooltipButton title="Удалить" handler={() => {}} icon={<DeleteIcon />} />
+              </>
+            )}
+          </Box>
           {column.title}
         </Button>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <InfoButton type="small" onClick={(): void => {}} />
-          <EditButton type="small" onClick={(): void => {}} />
-          <DeleteButton type="small" onClick={(): void => {}} />
-          <AddButton type="small" onClick={(): void => {}} />
-        </Box>
-        <Typography sx={{ mb: 0.5 }} color="text.secondary">
-          {column.order}
-        </Typography>
+        <AddButton type="small" onClick={(): void => {}}>
+          {'Добавить задачу'}
+        </AddButton>
+        <Typography>{column.order}</Typography>
       </CardContent>
     </Card>
   );
