0cce3631a40e34fbfea9d0602f860f18d5ddda1a
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-11-29 15:54:21 +0300

    feat: change url backend

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'https://projectmanagement.herokuapp.com';
+export const API_URL = 'https://project-management-6bop.onrender.com/';
 export const TOKEN = 'token';
 export const USER_ID = 'userId';
