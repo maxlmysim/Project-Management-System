7ee30e54b3edd3e348c6dc1c55beb594c9d91371
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-12-01 18:52:07 +0300

    fix: fix api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,3 @@
-// export const API_URL = 'https://project-management-6bop.onrender.com/';
-export const API_URL = 'http://127.0.0.1:5000/';
-
+export const API_URL = 'https://project-management-6bop.onrender.com/';
 export const TOKEN = 'token';
 export const USER_ID = 'userId';
