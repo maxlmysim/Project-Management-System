6d56d5e1c2c4f565960ce8da60fc117c5bdc3629
jocker-py <buladzislau@gmail.com>
2022-12-02 21:22:23 +0300

    fix: fix bug with notfound page after signIn

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,10 +2,16 @@ import React, { FC } from 'react';
 import { Avatar, Box, Container, Typography } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import AuthField from '../components/AuthField';
-import { signIn } from '../store/authSlice';
+import { authSelector, signIn } from '../store/authSlice';
 import { signInFieldList } from '../constants/modalField';
+import { useAppSelector } from '../hooks/storeHooks';
+import { useNavigate } from 'react-router-dom';
+import { AppRoutes } from '../constants/routes';
 
 const LoginPage: FC = () => {
+  const { isLogin } = useAppSelector(authSelector);
+  const navigate = useNavigate();
+  isLogin && navigate(AppRoutes.WELCOME);
   return (
     <Container component="main">
       <Box alignItems="center" sx={{ display: 'flex', flexDirection: 'column', marginTop: '3rem' }}>
