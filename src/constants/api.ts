12ae0868c6ff358ef4baf5da0042dfee91403554
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-12-06 21:37:40 +0300

    fix: change url server

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
 // export const API_URL = 'https://project-management-6bop.onrender.com/';
-export const API_URL = 'http://localhost:5000/';
-// export const API_URL = 'https://project-management-be.onrender.com/';
+// export const API_URL = 'http://localhost:5000/';
+export const API_URL = 'https://project-management-be.onrender.com/';
 export const TOKEN = 'token';
 export const USER_ID = 'userId';
